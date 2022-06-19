081abbad3039006d52a8122c56552ee11378b7e1
nico1008 <nikiapple1008@icloud.com>
2022-06-20 02:58:48 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useColorMode } from '@chakra-ui/color-mode';
 import { IconButton } from '@chakra-ui/button';
 import { Flex,VStack,Heading,Spacer }  from "@chakra-ui/layout";
-import { FaSun, FaMoon, FaGithub,FaTelegram,FaVk } from 'react-icons/fa'
+import { FaSun, FaMoon, FaGithub,FaTelegram,FaBehance } from 'react-icons/fa'
 import Header from './components/Header';
 import Social from './components/Social';
 import Profile from './components/Profile';
@@ -14,21 +14,21 @@ function App() {
 
   return(
     <VStack p={5}>
-      <Flex w="100%">
-        <Heading ml ="8" mt={"1.5"} size="lg" fontWeight='semibold' color="violet">
+      <Flex w="100%" mb="20px">
+        <Heading ml ="8" mt={"1.5"} size="lg" fontWeight='Bold' color="violet">
           Nico1008
         </Heading>
       
       <Spacer>
 
       </Spacer>
-      <IconButton ml={5} size="lg" bgGradient="linear(to-b, violet,blue.500 )" icon={<FaVk/>} isRound='true' onClick  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
-      <IconButton ml={5} size="lg" bgGradient="linear(to-b, violet,blue.500 )" icon={<FaGithub/>} isRound='true' onClick  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
-      <IconButton ml={5} size="lg" bgGradient="linear(to-b, violet,blue.500 )" icon={<FaTelegram/>} isRound='true' onClick  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
+      <IconButton ml={5} size="lg" bgGradient="linear(to-b, violet,blue.500 )" icon={<FaBehance/>} isRound='true' onClick={()=>window.open("https://www.behance.net/nico_1008")}  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
+      <IconButton ml={5} size="lg" bgGradient="linear(to-b, violet,blue.500 )" icon={<FaGithub/>} isRound='true' onClick={()=>window.open("https://github.com/nico1008")}  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
+      <IconButton ml={5} size="lg" bgGradient="linear(to-b, violet,blue.500 )" icon={<FaTelegram/>} isRound='true' onClick={()=>window.open("https://t.me/nico_1008k")}  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
       <IconButton ml={20} size="lg" bgGradient="linear(to-b, violet,blue.500 )"  icon={isDark ? <FaSun/> :<FaMoon/>} isRound='true'  _hover={{ bgGradient: 'linear(to-t,green.400,green.200)',}} onClick={toggleColorMode}></IconButton>
-      </Flex>
+      </Flex  >
 
-      <Header></Header>
+      <Header ></Header>
       <Social></Social>
       <Profile></Profile>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { useColorMode } from '@chakra-ui/color-mode';
 import { useMediaQuery } from '@chakra-ui/media-query';
 import {  Stack, Circle, Flex, Box, Text,Spacer  } from '@chakra-ui/layout';
 import { Button, Image } from '@chakra-ui/react'
-import { FaGithub,FaTelegram,FaVk } from 'react-icons/fa'
+import { FaGithub,FaTelegram,FaBehance } from 'react-icons/fa'
 import { IconButton } from '@chakra-ui/button';
 
 export default function Header() {
@@ -14,34 +14,33 @@ export default function Header() {
      const [isNotSmallerScreen]=useMediaQuery("(min-width:600px)");
 
      return (
-          <Stack>
-               <Circle  position="absolute" mt={"100px"} bg="pink" opacity={isNotSmallerScreen ? "0.1": "0"} w="300px" h="300px" alignSelf={"flex-end"}></Circle>
-               <Flex direction ={isNotSmallerScreen ? "row" : "column"}
-                    spacing="200px" p={isNotSmallerScreen ? "32" : "0"}
+          <Stack >
+               <Circle  position="absolute" mt={"100px"} mr="50px" bg="pink" opacity={isNotSmallerScreen ? "0.1": "0"} w="300px" h="300px" alignSelf={"flex-end"}></Circle>
+               <Flex backgroundColor={isDark ? "#2953A6" : "#00FF80"} borderRadius="20px"  direction ={isNotSmallerScreen ? "row" : "column"}
+                    spacing="200px" p={isNotSmallerScreen ? "32" : "4"} pl={isNotSmallerScreen ? "16" : "4"}
                     alignSelf="flex-start">
-                    <Box mt={isNotSmallerScreen ? "0": 16} align='flex-start'>
+                    <Box mt={isNotSmallerScreen ? "0": 16} pr={isNotSmallerScreen ? "150px": "0"} align='flex-start'>
                          <Text fontSize={"5xl"} fontWeight="semibold"  >
                               Hello, my name is 
                          </Text>
                          <Text fontSize={"7xl"} fontWeight="bold" bgGradient="linear(to-r, coral, violet, purple)" bgClip='text'>
-                              Nico
+                              Nicola Ivanov
                          </Text >
-                         <Text pt={"5"} fontSize={"2xl"} fontWeight="semibold" color={isDark ? "gray.200" : "gray.500"} >
-                         I am a young developer, studying at MTUCI. I am a young <br></br> developer, studying at MTUCI.
-                         I am a young developer, <br></br>studying at MTUCI. I am a young developer, studying at MTUCI. 
+                         <Text pt={"5"}   fontSize={"2xl"} fontWeight="semibold" color={isDark ? "gray.200" : "black"} >
+                         I am a young developer, studying at MTUCI. I work on  various C++, Java, Python, Mathlab,<br></br>Web design and Art projects.
                          </Text>
                          <Spacer>
 
                          </Spacer>
                          <Button mt={8}  bgGradient="linear(to-r, violet,blue.500 )"  
                          _hover={{ bgGradient: 'linear(to-l, red.500, yellow.500)',}}
-                         onClick={()=>window.open("https://google.com")}>
+                         onClick={()=>window.open("https://vk.com/karkron")}>
                               Contact me  
                          </Button>
                          
-                         <IconButton mt={8} ml={8} size="md"  icon={<FaVk/>} isRound='true' onClick  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
-                         <IconButton mt={8} ml={8} size="md"  icon={<FaGithub/>} isRound='true' onClick  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
-                         <IconButton mt={8} ml={8} size="md"  icon={<FaTelegram/>} isRound='true' onClick  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
+                         <IconButton mt={8} ml={8} size="md"  icon={<FaBehance/>} isRound='true' onClick={()=>window.open("https://www.behance.net/nico_1008")}  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
+                         <IconButton mt={8} ml={8} size="md"  icon={<FaGithub/>} isRound='true' onClick={()=>window.open("https://github.com/nico1008")}  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
+                         <IconButton mt={8} ml={8} size="md"  icon={<FaTelegram/>} isRound='true' onClick={()=>window.open("https://t.me/nico_1008k")}  _hover={{ bgGradient: 'linear(to-t, #D91E98, #F2226E)',}}></IconButton>
                     </Box>
                     <Image alignSelf="center" mt={isNotSmallerScreen ? "0": "10"} mb = {isNotSmallerScreen ? "0" : "10"} borderRadius='full'
                     backgroundColor={"transparent"} boxShadow="lg" boxSize={"300px"} opacity="1" border='8px solid black'
